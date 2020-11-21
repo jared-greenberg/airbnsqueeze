2e074941cc5e1f0404de96da78cd25996555f141
jared-greenberg <jared.p.greenberg@gmail.com>
2020-11-20 20:43:42 -0500

    Search Bar Styling

--- frontend/components/search/search_form.jsx
+++ frontend/components/search/search_form.jsx
@@ -19,40 +19,39 @@ class SearchForm extends React.Component {
 
   render () {
     return (
-    <form id="search-form" onSubmit={this.handleSubmit}>
-      <div className="location-field">
-        <label htmlFor="location">Location</label>
-        <input type="text" value={this.state.location}
-        placeholder="Where are you going?"
-        onChange = {this.updateField("location")}/>
-      </div>
-      <div className="calendars">
-          <label id="checkin">Check in</label>  
-          <label id="checkout">Check out</label>
-          <DateRangePicker
-            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-            startDateId="search_start" // PropTypes.string.isRequired,
-            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-            endDateId="search_end" // PropTypes.string.isRequired,
-            onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-            onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-            startDatePlaceholderText="Add Dates"
-            endDatePlaceholderText="Add Dates"
-            startDateAriaLabel="Check In"
-            endDateAriaLabel="Check Out"
-            displayFormat="MMM D"
-            noBorder={true}
-          />
-      </div>
-        <div className="guests">
-            <p id="guest-label">Guests</p>
-            <h3>Add guests</h3>
+    <div class="search-form-wrapper">
+      <form id="search-form" onSubmit={this.handleSubmit}>
+        <div className="location-field">
+          <label htmlFor="location">Location</label>
+          <input type="text" value={this.state.location}
+          placeholder="Where are you going?"
+          onChange = {this.updateField("location")}/>
         </div>
-        <div id="search-bar-button"><i className="fas fa-search"></i></div>
+        <div className="calendars"> 
+            <label id="checkin-label">Check in</label>
+            <label id="checkout-label">Check out</label>
+            <DateRangePicker
+              startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+              startDateId="search_start" // PropTypes.string.isRequired,
+              endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+              endDateId="search_end" // PropTypes.string.isRequired,
+              onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+              focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+              onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+              startDatePlaceholderText="Add dates"
+              endDatePlaceholderText="Add dates"
+              displayFormat="MMM D"
+              noBorder={true}
+            />
+        </div>
         
-      
-    </form>
+          <div className="guests">
+              <label id="guests-label">Guests</label>
+              <h3>Add guests</h3>
+          </div>
+      </form>
+      <div id="search-bar-button"><i className="fas fa-search"></i></div>
+    </div>
     )
   }
 }
