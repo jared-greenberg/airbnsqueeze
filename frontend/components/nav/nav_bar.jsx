3aad58ca79e225231f5cd27baff2984444ce44d0
jared-greenberg <jared.p.greenberg@gmail.com>
2020-11-21 00:23:35 -0500

    Fixing issues with date picker and heroku

--- frontend/components/nav/nav_bar.jsx
+++ frontend/components/nav/nav_bar.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import DropDown from './drop_down';
 import { Link } from 'react-router-dom';
+import SearchFormContainer from '../search/search_form_container';
 
 export default (props) => {
   const fixed = props.fixed ? "fixed-nav" : "";
   return (
-    <nav className={`${fixed} normal-nav`}>
-      <Link to="/"><h1 id="logo">airbnsqueeze</h1></Link>
-      <button id="search-button">
-        Start your search
-        <div id="search-badge"><i className="fas fa-search"></i></div>
-      </button>
-      <DropDown />
-    </nav>
+      <header className="navWrapper">
+        <nav className={`${fixed} normal-nav`}>
+          <Link to="/"><h1 id="logo">airbnsqueeze</h1></Link>
+          <button id="search-button">
+            Start your search
+            <div id="search-badge"><i className="fas fa-search"></i></div>
+          </button>
+          <DropDown />
+        </nav>
+          <SearchFormContainer />
+      </header>
   )
 }
