55dd4116c00717f64c5e45d19af24f7dd663a882
jared-greenberg <jared.p.greenberg@gmail.com>
2020-12-27 17:55:33 -0500

    Use a less specific circle marker for the show map

--- frontend/components/map/marker_manager.js
+++ frontend/components/map/marker_manager.js
@@ -29,13 +29,29 @@ class MarkerManager {
     delete this.markers[markerId]
   }
 
-  createMarker(listing, markerClickHandler){
+  createMarker(listing, markerClickHandler, type){
     const coords = {lat: listing.latitude, lng: listing.longitude}
     const label = {
       url: window.marker,
       scaledSize: new google.maps.Size(50, 30)
     }
     if (!coords.lat || !coords.lng) { return }
+
+    if (type === "show") {
+      const circ = new google.maps.Circle({
+        strokeColor: "#044241",
+        strokeOpacity: 0.5,
+        strokeWeight: 2,
+        fillColor: "#044241",
+        fillOpacity: 0.35,
+        map: this.map,
+        center: coords,
+        radius: 700,
+      })
+      this.markers[listing.id] = circ;
+      return;
+    }
+
     const marker = new google.maps.Marker({
       position: coords,
       map: this.map,
