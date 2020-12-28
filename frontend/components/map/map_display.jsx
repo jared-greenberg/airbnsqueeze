28a37d9177350893df9f3bdd5127c52e82de2bb0
jared-greenberg <jared.p.greenberg@gmail.com>
2020-12-27 20:39:13 -0500

    Fix body overflow x and adjust zoom when looking at all listings

--- frontend/components/map/map_display.jsx
+++ frontend/components/map/map_display.jsx
@@ -22,7 +22,7 @@ class MapDisplay extends React.Component {
     }
     else {
       options.center = { lat: 40.227746, lng: - 97.250879 }
-      options.zoom = 4.10
+      options.zoom = 3.9;
 
     }
     return options;
