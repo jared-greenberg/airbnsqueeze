82204313b232ce70847c8a473095798d914739ce
jared-greenberg <jared.p.greenberg@gmail.com>
2021-01-19 21:10:04 -0500

    Reset photos

--- frontend/components/listing/listing_index.jsx
+++ frontend/components/listing/listing_index.jsx
@@ -31,8 +31,7 @@ class ListingIndex extends React.Component {
 
     render() {
       const listingIndexItems = this.props.listings.map(listing => {
-        // listing = {listing}
-        return <ListingIndexItem key={listing.id} listing={this.props.listings[0]} 
+        return <ListingIndexItem key={listing.id} listing={listing} 
                   history={this.props.history} 
                   amenities={this.props.amenities}
                    />
